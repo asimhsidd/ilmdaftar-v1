fbb23312ac570fcf9c68ed33dc836ba007957178
Rayaan Danish <rayaandanish@apples-MBP.lan>
2026-04-20 07:23:34 +0400

    Update base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: process.env.NODE_ENV === 'production' ? '/Islamic-Knowledge-Manager/' : '/',
+    base: process.env.NODE_ENV === 'production' ? '/ilmdaftar.github.io/' : '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
